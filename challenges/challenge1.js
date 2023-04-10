d4b42aa37d4301adb882bd5265bbc58e9ec9f968
alexa-whitney <alexawhitney@gmail.com>
2023-04-09 18:32:13 -0600

    completed challenge 1

--- challenges/challenge1.js
+++ challenges/challenge1.js
@@ -6,14 +6,26 @@
  * 1. Read over the code that follows. In what order will the outputs "Step 1",
  *    "Step 2", and "Step 3" be printed? How do you know?
  * 
+ *    "Step 1" will be printed first, followed by "Step 3", and then "Step 2". 
+ *    The console.log() statements for "Step 1" and "Step 3" are outside of the snooze() 
+ *    function and will execute immediately, while the console.log() statement for "Step 2" 
+ *    is inside the snooze() function and will execute after a 2 second delay due to the 
+ *    setTimeout() function.
  * 
  * 2. Run this code using `node challenge1.js`. In what order were the steps
  *    printed?
  * 
+ *    Step 1
+ *    Step 3
+ *    Step 2
  * 
  * 3. Change the delay time in the `snooze` function from 2000 ms to 0. In what
  *    order will the steps be printed now? Why? Re-run the code again to verify
  *    your expectation. Were you correct?
+ * 
+ *    The same order. Even though the delay was changed from 2000ms to 0, the setTimeout() 
+ *    function is still an asynchronous function and will be executed asynchronously, even 
+ *    if the delay time is set to 0.
  * 
  *******************************************************************************
  */
@@ -22,7 +34,7 @@
 function snooze(action) {
     setTimeout(function() {
       action();
-    }, 2000);
+    }, 0);
 }
 console.log('Step 1');
 
